3330beef2bf0594c1cfa7fb2b5ab8d9a8d79f153
amir-zeldes <amir.zeldes@georgetown.edu>
2016-05-27 16:11:41 -0400

    V1.3.1   * Import now tolerates schema-annotated nodes, which are treated as plain spans

--- _version.py
+++ _version.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 __author__ = "Amir Zeldes"
 __copyright__ = "Copyright 2015-2016, Amir Zeldes"
 __license__ = "MIT License"
