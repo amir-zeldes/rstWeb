8366eceae37de7074df4c5f70db939cac9b7ad2a
amir-zeldes <amir.zeldes@georgetown.edu>
2018-11-13 20:04:16 -0500

    Implement signal update and delete in SQL   * Deleting nodes, documents and project deletes associated signals   * Up/down-ticking node IDs due to re-segmenting also updates signal source column   * Cloning documents for assignments and deleting assignment also reflected in rst_signals   * Resetting document to import state also resets signals to import state

--- modules/rstweb_sql.py
+++ modules/rstweb_sql.py
@@ -61,6 +61,7 @@ def setup_db():
 
 	initialize_settings()
 
+
 def update_schema():
 	dbpath = os.path.dirname(os.path.realpath(__file__)) + os.sep +".."+os.sep+"rstweb.db"
 	conn = sqlite3.connect(dbpath)
@@ -271,6 +272,7 @@ def get_rst_rels(doc,project):
 def get_docs_by_project(user):
 	return generic_query("SELECT doc, project, user FROM docs WHERE user=? ORDER BY project, doc COLLATE NOCASE",(user,))
 
+
 def get_all_docs_by_project():
 	return generic_query("SELECT DISTINCT doc, project FROM docs ORDER BY project, doc COLLATE NOCASE",())
 
@@ -278,11 +280,14 @@ def get_all_docs_by_project():
 def add_node(node_id,left,right,parent,rel_name,text,node_kind,doc,project,user):
 	generic_query("INSERT INTO rst_nodes VALUES(?,?,?,?,?,?,?,?,?,?,?)", (node_id,left,right,parent,0,node_kind,text,rel_name,doc,project,user))
 
+
 def get_all_projects():
 	return generic_query("SELECT DISTINCT project FROM projects ORDER BY project",())
 
+
 def create_project(project_name):
 	generic_query("INSERT OR IGNORE INTO projects (project) VALUES (?)",(project_name,))
+
 
 def update_parent(node_id,new_parent_id,doc,project,user):
 	prev_parent = get_parent(node_id,doc,project,user)
@@ -304,21 +309,27 @@ def update_parent(node_id,new_parent_id,doc,project,user):
 			delete_node(prev_parent,doc,project,user)
 
 
-# Returns the multinuclear relation with which a multinuc is currently dominating its children
 def get_multirel(node_id,exclude_child,doc,project,user):
+	"""
+	Returns the multinuclear relation with which a multinuc is currently dominating its children
+	"""
+
 	rel_row = generic_query("SELECT rst_nodes.relname FROM rst_nodes JOIN rst_relations ON rst_nodes.relname = rst_relations.relname and rst_nodes.doc = rst_relations.doc and rst_nodes.project = rst_relations.project WHERE rst_relations.reltype = 'multinuc' and rst_nodes.parent=? and not rst_nodes.id=? and rst_nodes.doc=? and rst_nodes.project=? and rst_nodes.user=?",(node_id,exclude_child,doc,project,user))
 	if len(rel_row) > 0:
 		return rel_row[0][0]
 	else:
 		return get_def_rel("multinuc",doc,project)
 
+
 def get_parent(node_id,doc,project,user):
 	parent_row = generic_query("SELECT parent FROM rst_nodes WHERE id=? and doc=? and project=? and user=?",(node_id,doc,project,user))
 	return parent_row[0][0]
 
+
 def get_rel(node_id,doc,project,user):
 	rel_row = generic_query("SELECT relname FROM rst_nodes WHERE id=? and doc=? and project=? and user=?",(node_id,doc,project,user))
 	return rel_row[0][0]
+
 
 def get_kind(node_id,doc,project,user):
 	if node_id == "0":
@@ -326,6 +337,7 @@ def get_kind(node_id,doc,project,user):
 	else:
 		rel_row = generic_query("SELECT kind FROM rst_nodes WHERE id=? and doc=? and project=? and user=?",(node_id,doc,project,user))
 		return rel_row[0][0]
+
 
 def update_rel(node_id,new_rel,doc,project,user):
 	parent_id = get_parent(node_id,doc,project,user)
@@ -348,22 +360,27 @@ def update_rel(node_id,new_rel,doc,project,user):
 	else:
 		generic_query("UPDATE rst_nodes SET relname=? WHERE id=? and doc=? and project=? and user=?",(new_rel,node_id,doc,project,user))
 
+
 def count_children(node_id,doc,project,user):
 	count = generic_query("SELECT count(*) FROM rst_nodes WHERE parent=? and doc=? and project=? and user=?",(node_id,doc,project,user))
 	return int(count[0][0])
+
 
 def count_multinuc_children(node_id,doc,project,user):
 	count = generic_query("SELECT count(rst_nodes.id) FROM rst_nodes JOIN rst_relations ON rst_nodes.relname = rst_relations.relname and rst_nodes.doc = rst_relations.doc and rst_nodes.project = rst_relations.project WHERE reltype = 'multinuc' and parent=? and rst_nodes.doc=? and rst_nodes.project=? and user=?",(node_id,doc,project,user))
 	return int(count[0][0])
 
+
 def get_multinuc_children_lr(node_id,doc,project,user):
 	lr = generic_query("SELECT min(rst_nodes.left), max(rst_nodes.right) FROM rst_nodes JOIN rst_relations ON rst_nodes.relname = rst_relations.relname and rst_nodes.doc = rst_relations.doc and rst_nodes.project = rst_relations.project WHERE reltype = 'multinuc' and parent=? and rst_nodes.doc=? and rst_nodes.project=? and user=?",(node_id,doc,project,user))
 	return [int(lr[0][0]),int(lr[0][1])]
+
 
 def get_multinuc_children_lr_ids(node_id,left,right,doc,project,user):
 	id_left = generic_query("SELECT id FROM rst_nodes JOIN rst_relations ON rst_nodes.relname = rst_relations.relname and rst_nodes.doc = rst_relations.doc and rst_nodes.project = rst_relations.project WHERE reltype = 'multinuc' and parent=? and rst_nodes.left=? and rst_nodes.doc=? and rst_nodes.project=? and user=? ORDER BY rst_nodes.left",(node_id,left,doc,project,user))
 	id_right = generic_query("SELECT id FROM rst_nodes JOIN rst_relations ON rst_nodes.relname = rst_relations.relname and rst_nodes.doc = rst_relations.doc and rst_nodes.project = rst_relations.project WHERE reltype = 'multinuc' and parent=? and rst_nodes.right=? and rst_nodes.doc=? and rst_nodes.project=? and user=? ORDER BY rst_nodes.left",(node_id,right,doc,project,user))
 	return id_left[0][0],id_right[0][0]
+
 
 def count_span_children(node_id,doc,project,user):
 	count = generic_query("SELECT count(id) FROM rst_nodes WHERE relname = 'span' and parent=? and rst_nodes.doc=? and rst_nodes.project=? and user=?",(node_id,doc,project,user))
@@ -381,6 +398,7 @@ def get_rel_type(relname,doc,project):
 	else:
 		return generic_query("SELECT reltype from rst_relations WHERE relname=? and doc=? and project=?",(relname,doc,project))[0][0]
 
+
 def delete_node(node_id,doc,project,user):
 	if node_exists(node_id,doc,project,user):
 		parent = get_parent(node_id,doc,project,user)
@@ -391,6 +409,7 @@ def delete_node(node_id,doc,project,user):
 				if len(child[0])>0:
 					update_parent(child[0],"0",doc,project,user)
 			generic_query("DELETE FROM rst_nodes WHERE id=? and doc=? and project=? and user=?",(node_id,doc,project,user))
+			generic_query("DELETE FROM rst_signals WHERE source=? and doc=? and project=? and user=?",(node_id,doc,project,user))
 		if not parent=="0":
 			if not count_children(parent,doc,project,user)>0:
 				delete_node(parent,doc,project,user)
@@ -409,22 +428,31 @@ def insert_parent(node_id,new_rel,node_kind,doc,project,user):
 	update_parent(node_id,new_parent,doc,project,user)
 	update_rel(node_id,new_rel,doc,project,user)
 
+
 def reset_rst_doc(doc,project,user):
 	generic_query("DELETE FROM rst_nodes WHERE doc=? and project=? and user=?",(doc,project,user))
 	generic_query("""INSERT INTO rst_nodes (id, left, right, parent, depth, kind, contents, relname, doc, project, user)
 	              SELECT id, left, right, parent, depth, kind, contents, relname, doc, project, '""" + user + "' FROM rst_nodes WHERE doc=? and project=? and user='_orig'""",(doc,project))
+	generic_query("DELETE FROM rst_signals WHERE doc=? and project=? and user=?",(doc,project,user))
+	generic_query("""INSERT INTO rst_signals (source, type, subtype, tokens, doc, project, user)
+	              SELECT source, type, subtype, tokens, doc, project, '""" + user + "' FROM rst_signals WHERE doc=? and project=? and user='_orig'""",(doc,project))
+
 
 def get_children(parent,doc,project,user):
 	return generic_query("SELECT id from rst_nodes WHERE parent=? and doc=? and project=? and user=?",(parent,doc,project,user))
 
+
 def get_max_node_id(doc,project,user):
 	return generic_query("SELECT max(CAST (id as decimal)) as max_id from rst_nodes WHERE doc=? and project=? and user=?",(doc,project,user))[0][0]
+
 
 def get_max_right(doc,project,user):
 	return generic_query("SELECT max(right) as max_right from rst_nodes WHERE doc=? and project=? and user=?",(doc,project,user))[0][0]
 
+
 def get_users(doc,project):
 	return generic_query("SELECT user from rst_nodes WHERE doc=? and project=? and not user='_orig'",(doc,project))
+
 
 def generic_query(sql,params):
 	dbpath = os.path.dirname(os.path.realpath(__file__)) + os.sep +".."+os.sep+"rstweb.db"
@@ -501,15 +529,18 @@ def get_export_string(doc, project, user):
 </rst>'''
 	return rst_out
 
+
 def delete_document(doc,project):
 	generic_query("DELETE FROM rst_nodes WHERE doc=? and project=?",(doc,project))
 	generic_query("DELETE FROM rst_relations WHERE doc=? and project=?",(doc,project))
+	generic_query("DELETE FROM rst_signals WHERE doc=? and project=?",(doc,project))
 	generic_query("DELETE FROM docs WHERE doc=? and project=?",(doc,project))
 
 
 def delete_project(project):
 	generic_query("DELETE FROM rst_nodes WHERE project=?",(project,))
 	generic_query("DELETE FROM rst_relations WHERE project=?",(project,))
+	generic_query("DELETE FROM rst_signals WHERE project=?",(project,))
 	generic_query("DELETE FROM docs WHERE project=?",(project,))
 	generic_query("DELETE FROM projects WHERE project=?",(project,))
 
@@ -535,11 +566,13 @@ def get_tok_map(doc,project,user):
 
 	return all_tokens
 
+
 def push_up(push_above_this_seg,doc,project,user):
 	ids_above_push = generic_query("SELECT id from rst_nodes WHERE CAST(id as int) > ? and doc=? and project=? and user=? ORDER BY CAST(id as int) DESC",(push_above_this_seg,doc,project,user))
 	for row in ids_above_push: #Do this row-wise to avoid sqlite unique constraint behavior
 		id_to_increment = row[0]
 		generic_query("UPDATE rst_nodes set id = CAST((CAST(id as int) + 1) as text) WHERE id=? and doc=? and project=? and user=?",(id_to_increment,doc,project,user))
+		generic_query("UPDATE rst_signals set source = CAST((CAST(source as int) + 1) as text) WHERE source=? and doc=? and project=? and user=?",(id_to_increment,doc,project,user))
 	generic_query("UPDATE rst_nodes set parent = CAST((CAST(parent as int) + 1) as text) WHERE CAST(parent as int)>? and doc=? and project=? and user=?",(push_above_this_seg,doc,project,user))
 	generic_query("UPDATE rst_nodes set left = left + 1 WHERE left>? and doc=? and project=? and user=?",(push_above_this_seg,doc,project,user))
 	generic_query("UPDATE rst_nodes set right = right + 1 WHERE right>? and doc=? and project=? and user=?",(push_above_this_seg,doc,project,user))
@@ -550,6 +583,7 @@ def push_down(push_above_this_seg,doc,project,user):
 	for row in ids_above_push: #Do this row-wise to avoid sqlite unique constraint behavior
 		id_to_decrement = row[0]
 		generic_query("UPDATE rst_nodes set id = CAST((CAST(id as int) - 1) as text) WHERE id=? and doc=? and project=? and user=?",(id_to_decrement,doc,project,user))
+		generic_query("UPDATE rst_signals set source = CAST((CAST(source as int) - 1) as text) WHERE source=? and doc=? and project=? and user=?",(id_to_decrement,doc,project,user))
 	generic_query("UPDATE rst_nodes set parent = CAST((CAST(parent as int) - 1) as text) WHERE CAST(parent as int)>? and doc=? and project=? and user=?",(push_above_this_seg,doc,project,user))
 	generic_query("UPDATE rst_nodes set left = left - 1 WHERE left>? and doc=? and project=? and user=?",(push_above_this_seg,doc,project,user))
 	generic_query("UPDATE rst_nodes set right = right - 1 WHERE right>? and doc=? and project=? and user=?",(push_above_this_seg,doc,project,user))
@@ -584,6 +618,7 @@ def get_split_text(tok_num,doc,project,user):
 
 	return final
 
+
 def update_seg_contents(id,contents,doc,project,user):
 	generic_query("UPDATE rst_nodes set contents=? WHERE id=? and doc=? and project=? and user=?",(contents,id,doc,project,user))
 
@@ -594,6 +629,7 @@ def get_seg_contents(id,doc,project,user):
 
 def add_seg(id,contents,doc,project,user):
 	generic_query("INSERT INTO rst_nodes VALUES(?,?,?,?,?,?,?,?,?,?,?)", (id,id,id,"0","0","edu",contents,get_def_rel("rst",doc,project),doc,project,user))
+
 
 def merge_seg_forward(last_tok_num,doc,project,user):
 	tok_seg_map = get_tok_map(doc,project,user)
@@ -614,6 +650,7 @@ def merge_seg_forward(last_tok_num,doc,project,user):
 		update_parent(child[0],"0",doc,project,user)
 	#remove it from the database
 	generic_query("DELETE FROM rst_nodes WHERE id=? and doc=? and project=? and user=?",(str(int(seg_to_merge_forward)+1),doc,project,user))
+	generic_query("DELETE FROM rst_signals WHERE source=? and doc=? and project=? and user=?",(str(int(seg_to_merge_forward)+1),doc,project,user))
 	push_down(int(seg_to_merge_forward),doc,project,user)
 
 
@@ -631,30 +668,53 @@ def copy_doc_to_user(doc, project, user):
 	cur.execute("INSERT INTO docs VALUES (?,?,?)", (doc,project,user))
 	conn.commit()
 
+	signals_to_copy = generic_query("SELECT source, type, subtype, tokens, doc, project FROM rst_signals WHERE doc=? and project=? and user='_orig'", (doc,project))
+	copy = []
+	for row in signals_to_copy:
+		row += (user,)
+		copy += (row,)
+
+	if len(copy)>0:
+		dbpath = os.path.dirname(os.path.realpath(__file__)) + os.sep +".."+os.sep+"rstweb.db"
+		conn = sqlite3.connect(dbpath)
+		cur = conn.cursor()
+		cur.executemany('INSERT INTO rst_nodes VALUES(?,?,?,?,?,?,?,?,?,?,?)', copy)
+		cur.execute("INSERT INTO docs VALUES (?,?,?)", (doc,project,user))
+		conn.commit()
+
+
 def get_assigned_users():
 	return generic_query("SELECT DISTINCT user from docs where not user = '_orig' ORDER BY user COLLATE NOCASE",())
+
 
 def get_assignments(user):
 	return generic_query("SELECT DISTINCT doc, project from docs where user=? ORDER BY user COLLATE NOCASE",(str(user),))
 
+
 def delete_doc_user_version(doc,project,user):
 	generic_query("DELETE FROM rst_nodes WHERE doc=? and project=? and user=?",(doc,project,user))
+	generic_query("DELETE FROM rst_signals WHERE doc=? and project=? and user=?",(doc,project,user))
 	generic_query("DELETE FROM docs WHERE doc=? and project=? and user=?",(doc,project,user))
+
 
 def get_node_lr(node_id,doc,project,user):
 	left =  generic_query("SELECT left FROM rst_nodes WHERE id=? and doc=? and project=? and user=?",(node_id,doc,project,user))[0][0]
 	right =  generic_query("SELECT right FROM rst_nodes WHERE id=? and doc=? and project=? and user=?",(node_id,doc,project,user))[0][0]
 	return [left,right]
 
+
 def delete_docs_for_user(user):
 	generic_query("DELETE FROM rst_nodes WHERE user=?",(user,))
+	generic_query("DELETE FROM rst_signals WHERE user=?",(user,))
 	generic_query("DELETE FROM docs WHERE user=?",(user,))
+
 
 def update_log(doc,project,user,logging,mode,time):
 	actions = logging.split(";")
 	for action in actions:
 		if len(action) > 1:
 			generic_query("INSERT INTO logging VALUES (?,?,?,?,?,?)",(doc,project,user,action,mode,time))
+
 
 def get_setting(setting):
 	schema = get_schema()
@@ -663,13 +723,16 @@ def get_setting(setting):
 	else:
 		return ""
 
+
 def save_setting(setting, svalue):
 	schema = get_schema()
 	if schema > 1:
 		generic_query("INSERT INTO settings VALUES (?,?)",(setting,svalue))
 
+
 def set_guidelines_url(project,guideline_url):
 	generic_query("UPDATE projects SET guideline_url=? where project=?",(guideline_url,project))
+
 
 def get_guidelines_url(project):
 	schema = get_schema()
@@ -682,9 +745,9 @@ def get_guidelines_url(project):
 		else:
 			return guidelines
 
+
 def clean_floating_nodes(doc, project, user):
-	sql = """SELECT id
-FROM rst_nodes
+	sql = """SELECT id FROM rst_nodes
 WHERE id NOT IN (SELECT parent FROM rst_nodes where not parent = 0 AND doc=? AND project=? AND user=?) AND
 NOT kind = 'edu' AND
 doc=? AND project=? AND user=?;"""
@@ -697,4 +760,6 @@ doc=? AND project=? AND user=?;"""
 
 		id_string = ",".join(ids_to_del)
 		del_query = "DELETE from rst_nodes where ID in ("+ id_string +") and doc=? and project=? and user=?;"
+		generic_query(del_query,(doc,project,user))
+		del_query = "DELETE from rst_signals where source in ("+ id_string +") and doc=? and project=? and user=?;"
 		generic_query(del_query,(doc,project,user))
