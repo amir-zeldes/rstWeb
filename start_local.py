906595b68e792f9fc87193fab4771fb5b0e2585e
Arne Neumann <github@arne.cl>
2018-11-15 18:10:58 +0100

    fix CORS for Javascript clients

--- start_local.py
+++ start_local.py
@@ -16,7 +16,7 @@ from structure import structure_main
 from segment import segment_main
 from admin import admin_main
 from quick_export import quickexp_main
-import cherrypy_cors
+
 from cherrypy.lib import file_generator
 try:
 	from StringIO import StringIO
@@ -76,7 +76,11 @@ class Root(object):
 		return admin_main("local","3",'local',**kwargs)
 
 
-cherrypy_cors.install()
+def CORS():
+    """Adds CORS header to response (allows Javascript clients to use the API)."""
+    cherrypy.response.headers["Access-Control-Allow-Origin"] = "*"
+
+
 dispatcher = create_api_dispatcher()
 
 current_dir = os.path.dirname(os.path.realpath(__file__)) + os.sep
@@ -90,12 +94,13 @@ api_conf = {
     '/': {
         'request.dispatch': dispatcher,
         'error_page.default': jsonify_error,
-        'cors.expose.on': True,
+        'tools.CORS.on': True,
         'request.show_tracebacks': True
     }
 }
 api_conf.update(conf)
 
+cherrypy.tools.CORS = cherrypy.Tool('before_handler', CORS)
 cherrypy.tree.mount(root=Root(), config=conf)
 cherrypy.tree.mount(root=APIController(), script_name='/api', config=api_conf)
 
