37951e0be0854e5207054d9d8f2d4f6253eb9157
Luke Gessler <ldg3fa@virginia.edu>
2019-01-16 22:46:22 -0500

    delete files used in old screenshot mechanism

--- modules/viewer/main/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: UTF-8 -*-
-import os
-
-from rstviewer import main, rstweb_classes, rstweb_reader, rstweb_sql
-from rstviewer.main import embed_rs3_image, rs3tohtml, rs3topng
-
-SOURCE_ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
-PACKAGE_ROOT_DIR = os.path.abspath(os.path.join(SOURCE_ROOT_DIR, '..'))
