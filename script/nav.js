a73965fdefc1816019268c3dd870767cb2f5ef07
Luke Gessler <ldg3fa@virginia.edu>
2019-02-12 13:16:35 -0500

    add image downloading anchor element to dom before clicking for firefox compat

--- script/nav.js
+++ script/nav.js
@@ -216,8 +216,11 @@ function do_screenshot(){
         var filename = document.getElementById('current_doc').value.replace(".rs3","");
         a.setAttribute('href', url);
         a.setAttribute('download', filename + ".png");
+        a.setAttribute('download', filename + ".png");
+        $(a).insertAfter($("#canvas"));
         a.click();
 
+        $(a).remove();
         offscreenDiv.remove();
     });
 }
