6dbdc0509df875b0256f257effcae29f32681636
amir-zeldes <amir.zeldes@georgetown.edu>
2020-06-03 15:24:59 -0400

    V3.0.2   * Bugfixes and performance improvements

--- _version.py
+++ _version.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-__version__ = "3.0.0"
+__version__ = "3.0.2"
 __author__ = "Amir Zeldes, Luke Gessler"
 __copyright__ = "Copyright 2015-2019, Amir Zeldes"
 __license__ = "MIT License"
